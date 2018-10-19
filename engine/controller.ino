0660973c85432d2ec7e3a147b3edf18d95c74af2
Alex Melkonyan <sasha.melkonyan@gmail.com>
2018-10-20 01:52:50 +0200

    Add controller to start and stop the e-bike engine

---
+++ engine/controller.ino
@@ -0,0 +1,14 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(A0, HIGH);
+  delay(10);
+  digitalWrite(A0, LOW);
+  delay(30);
+  
+}
